0628524165fee88a9938a363ab9242912e39cb37
isaac-w18 <isaacw1218@gmail.com>
2023-02-22 11:54:13 -0500

    Forms and calculators

--- mp03/mp03/ContentView.swift
+++ mp03/mp03/ContentView.swift
@@ -9,16 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var input: Double = 0.0
+    @State private var inputUnit = ""
+    @State private var outputUnit = ""
+    
+    let units = ["mL", "Liters", "Cups", "Pints", "Gallons"]
+    let conversions = ["mL": 1.0,
+                    "Liters": 1000.0,
+                    "Cups": 236.588,
+                    "Pints": 473.176,
+                    "Gallons": 3785.41]
+    
+    var output: Double {
+        let milileters = input * (conversions[inputUnit] ?? 0)
+        return milileters / (conversions[outputUnit] ?? 1.0)
+    }
     
     var body: some View {
-        Form {
-            Section {
-                TextField("Input Value",
-                          value: $input,
-                          format: Double)
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Input Value",
+                              value: $input,
+                              format: .number)
+                    Picker("Input Units", selection: $inputUnit) {
+                        Text("mL").tag("mL")
+                        Text("Liters").tag("Liters")
+                        Text("Cups").tag("Cups")
+                        Text("Pints").tag("Pints")
+                        Text("Gallons").tag("Gallons")
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Input Value and Unit")
+                }
+                
+                Section {
+                    Picker("Output Units", selection: $outputUnit) {
+                        Text("mL").tag("mL")
+                        Text("Liters").tag("Liters")
+                        Text("Cups").tag("Cups")
+                        Text("Pints").tag("Pints")
+                        Text("Gallons").tag("Gallons")
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Output Unit")
+                }
+                
+                Section {
+//                    TextField("\(output)",
+//                              value: output,
+//                              format: .number)
+                } header: {
+                    Text("Output")
+                }
             }
+            .navigationTitle("Volume Conversion")
         }
-        .navigationTitle("Volume Conversion")
     }
 }
 
